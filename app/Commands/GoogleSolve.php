a18720e7e8372661b32f466b26ee11c18a1c9a3b
Vitaliy Yushkevich <yushkevichv@gmail.com>
2021-02-06 20:04:25 +0300

    add progress bar visualisation

--- app/Commands/GoogleSolve.php
+++ app/Commands/GoogleSolve.php
@@ -30,8 +30,10 @@ class GoogleSolve extends Command
      */
     public function handle()
     {
+        $timeStart = microtime(true);
         // @todo move to param
         $inputFile = storage_path('input/a_example.txt');
+//        $inputFile = storage_path('input/b_read_on.txt');
         if (!is_file($inputFile)) {
             $this->error("invalid input file");
             die(1);
@@ -52,6 +54,9 @@ class GoogleSolve extends Command
             $this->outputResult();
             return true;
         });
+
+        $timeEnd = microtime(true);
+        $this->comment("Process solving take: ". ($timeEnd - $timeStart). " sec");
     }
 
     private function fillDataFromFile(string $filePath): void
@@ -61,17 +66,27 @@ class GoogleSolve extends Command
         $this->limits = new \App\Models\Limits(...$this->unpack(fgets($fileHandle)));
         $booksScore = $this->unpack(fgets($fileHandle));
 
+        $progressBar = $this->createProgressBar($this->limits->booksCount);
+
         for ($i = 0; $i < $this->limits->booksCount; $i++) {
             $this->books[] = new \App\Models\Book($i, (int) $booksScore[$i]);
+            $progressBar->advance();
         }
 
+        $progressBar->finish();
+        $this->comment("books filled");
+
         $this->libraries = collect([]);
+        $progressBar = $this->createProgressBar($this->limits->booksCount);
         for ($i = 0; $i < $this->limits->librariesCount; $i++) {
             $library = new \App\Models\Library($i, ...$this->unpack(fgets($fileHandle)));
             $library->setBooks(collect($this->books)->whereIn('id', $this->unpack(fgets($fileHandle))));
             $library->calculateScore($this->limits->daysLimit);
             $this->libraries->push($library);
+            $progressBar->advance();
         }
+        $progressBar->finish();
+        $this->comment("libraries filled");
 
         fclose($fileHandle);
     }
@@ -85,6 +100,7 @@ class GoogleSolve extends Command
     {
         $this->solution = new \App\Models\Solution();
 
+        $progressBar = $this->createProgressBar($this->libraries->count());
         while ($this->libraries->isNotEmpty() && $this->limits->daysLimit > 0) {
             $this->recalcLibraryScore();
             /** @var \App\Models\Library $library */
@@ -96,10 +112,22 @@ class GoogleSolve extends Command
             $this->solution->addLibrary($library, $this->limits->daysLimit);
             $this->limits->daysLimit -= $library->signupDays;
 
+            $progressBar->advance();
             if ($library->books->isEmpty()) {
                 continue;
             }
         }
+        $progressBar->finish();
+    }
+
+    private function createProgressBar($maxSteps) : \Symfony\Component\Console\Helper\ProgressBar
+    {
+        $progressBar = new \Symfony\Component\Console\Helper\ProgressBar($this->output, $maxSteps);
+        $progressBar->setEmptyBarCharacter('░'); // light shade character \u2591
+        $progressBar->setProgressCharacter('');
+        $progressBar->setBarCharacter('▓'); // dark shade character \u2593
+        $progressBar->setFormat(" %current%/%max% [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s% %memory:6s% \n");
+        return $progressBar;
     }
 
     private function recalcLibraryScore()
